53b944de90a6aa12057167f2fe5507b8ca6fc0b3
Rizwan Saputra <rizwansaputra700@gmail.com>
2026-06-26 12:46:30 +0700

    Add redirect for /docs/components

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/api/v1');
 });
+
+Route::get('/docs/components', function () {
+    return redirect('/api/v1');
+});
